8ea8b5ea1cc2342e745bf9ec88f0ec32d75140ca
Anwer Ullah <Anwer Ullah@DESKTOP-EB9HOQL>
2019-04-19 21:25:00 +0600

    navaigate-use

--- UdemyCourse-SPA/src/app/navbar/navbar.component.ts
+++ UdemyCourse-SPA/src/app/navbar/navbar.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { LoginService } from '../_services/login.service';
 import { AlertifyService } from '../_services/alertify.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
@@ -10,7 +11,7 @@ import { AlertifyService } from '../_services/alertify.service';
 export class NavbarComponent implements OnInit {
 
   model: any = {};
-  constructor(public loginService: LoginService, private alertify: AlertifyService) { }
+  constructor(public loginService: LoginService, private alertify: AlertifyService, private router: Router) { }
 
   ngOnInit() {
   }
@@ -20,16 +21,20 @@ export class NavbarComponent implements OnInit {
       // console.log('Logged in Successfully');
     }, error => {
         this.alertify.error(error);
-    } );
-  }
+    }, () => {
+      this.router.navigate(['/home']);
+    }
+     ) ; }
   loggedIn() {
     // const token = localStorage.getItem('token');
     // return !!token;
     return this.loginService.loggedIn();
+
   }
   logout() {
     localStorage.removeItem('token');
     this.alertify.message( 'logged out !!');
+    this.router.navigate(['/home']);
   }
 
 }
